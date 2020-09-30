7b7d2ee1f18dcbd9b93ff4dd74d5f030a2ca44a5
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2020-09-30 19:11:12 +0200

    feat(json-validation): add OpenAPI visitor
    Initialize a json-validation config for each API operation with a request body.
    
    Closes gravitee-io/issues#4293

---
+++ src/main/java/io/gravitee/policy/jsonvalidation/swagger/JsonValidationOAIOperationVisitor.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.policy.jsonvalidation.swagger;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import io.gravitee.policy.api.swagger.Policy;
+import io.gravitee.policy.api.swagger.v3.OAIOperationVisitor;
+import io.gravitee.policy.jsonvalidation.configuration.JsonValidationPolicyConfiguration;
+import io.swagger.v3.core.util.Json;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.parameters.RequestBody;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Optional;
+
+/**
+ * @author Florent CHAMFROY (florent.chamfroy at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+public class JsonValidationOAIOperationVisitor implements OAIOperationVisitor {
+
+    private final ObjectMapper mapper = new ObjectMapper();
+    {
+        mapper.configure(JsonGenerator.Feature.WRITE_NUMBERS_AS_STRINGS, true);
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+    }
+
+    @Override
+    /**
+     * openAPI has been parsed with the "resolveFully" option. As a consequence, all $ref have been replaced by proper definition.
+     */
+    public Optional<Policy> visit(io.swagger.v3.oas.models.OpenAPI openAPI, io.swagger.v3.oas.models.Operation operation) {
+
+        String jsonSchema = null;
+        final RequestBody requestBody = operation.getRequestBody();
+        if (requestBody != null && requestBody.getContent() != null && requestBody.getContent().get("application/json") != null) {
+            final Schema schema = requestBody.getContent().get("application/json").getSchema();
+            jsonSchema = Json.pretty(schema);
+        }
+        if (!StringUtils.isEmpty(jsonSchema)) {
+            JsonValidationPolicyConfiguration configuration = new JsonValidationPolicyConfiguration();
+            try {
+                Policy policy = new Policy();
+                policy.setName("json-validation");
+                configuration.setSchema(jsonSchema);
+                policy.setConfiguration(mapper.writeValueAsString(configuration));
+                return Optional.of(policy);
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
+        }
+        return Optional.empty();
+    }
+}
---
+++ src/test/java/io/gravitee/policy/jsonvalidation/swagger/JsonValidationOAIOperationVisitorTest.java
@@ -0,0 +1,118 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.policy.jsonvalidation.swagger;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.gravitee.policy.api.swagger.Policy;
+import io.swagger.v3.core.util.Json;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.media.MediaType;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.parameters.RequestBody;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.HashMap;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author Florent CHAMFROY (florent.chamfroy at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class JsonValidationOAIOperationVisitorTest {
+
+    private JsonValidationOAIOperationVisitor visitor = new JsonValidationOAIOperationVisitor();
+
+    @Test
+    public void operationWithoutRequestBody() {
+        Operation operationMock = mock(Operation.class);
+
+        when(operationMock.getRequestBody()).thenReturn(null);
+        Optional<Policy> policy = visitor.visit(mock(OpenAPI.class), operationMock);
+        assertFalse(policy.isPresent());
+    }
+
+    @Test
+    public void operationWithoutApplicationJsonRequestBody() {
+        Operation operationMock = mock(Operation.class);
+
+        Content content = mock(Content.class);
+        RequestBody requestBody = mock(RequestBody.class);
+        when(operationMock.getRequestBody()).thenReturn(requestBody);
+        when(requestBody.getContent()).thenReturn(content);
+        when(content.get("application/json")).thenReturn(null);
+
+        Optional<Policy> policy = visitor.visit(mock(OpenAPI.class), operationMock);
+        assertFalse(policy.isPresent());
+    }
+
+    @Test
+    public void operationWithEmptyRequestBody() {
+        Operation operationMock = mock(Operation.class);
+
+        MediaType applicationJson = mock(MediaType.class);
+        Content content = mock(Content.class);
+        RequestBody requestBody = mock(RequestBody.class);
+        when(operationMock.getRequestBody()).thenReturn(requestBody);
+        when(requestBody.getContent()).thenReturn(content);
+        when(content.get("application/json")).thenReturn(applicationJson);
+
+        try(MockedStatic<Json> theMock = Mockito.mockStatic(Json.class)) {
+            theMock.when(() -> Json.pretty(any(Schema.class))).thenReturn("");
+            Optional<Policy> policy = visitor.visit(mock(OpenAPI.class), operationMock);
+            assertFalse(policy.isPresent());
+        }
+    }
+
+    @Test
+    public void operationWithJsonRequestBody() throws Exception {
+        final String jsonSchema = "a beautiful json schema";
+
+        Operation operationMock = mock(Operation.class);
+
+        Schema schema = mock(Schema.class);
+        MediaType applicationJson = mock(MediaType.class);
+        Content content = mock(Content.class);
+        RequestBody requestBody = mock(RequestBody.class);
+        when(operationMock.getRequestBody()).thenReturn(requestBody);
+        when(requestBody.getContent()).thenReturn(content);
+        when(content.get("application/json")).thenReturn(applicationJson);
+        when(applicationJson.getSchema()).thenReturn(schema);
+
+        try(MockedStatic<Json> theMock = Mockito.mockStatic(Json.class)) {
+            theMock.when(() -> Json.pretty(any(Schema.class))).thenReturn(jsonSchema);
+
+            Optional<Policy> policy = visitor.visit(mock(OpenAPI.class), operationMock);
+            assertTrue(policy.isPresent());
+
+            String configuration = policy.get().getConfiguration();
+            assertNotNull(configuration);
+            HashMap readConfig = new ObjectMapper().readValue(configuration, HashMap.class);
+            assertEquals(jsonSchema, readConfig.get("schema"));
+        }
+    }
+}
